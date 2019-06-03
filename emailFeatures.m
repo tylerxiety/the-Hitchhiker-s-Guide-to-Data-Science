47d37fcd53e67ea93230224695126dc2cac546d3
Tyler <tylerxiety@gmail.com>
2019-06-03 12:57:01 +1000

    Add files via upload

---
+++ emailFeatures.m
@@ -0,0 +1,14 @@
+function x = emailFeatures(word_indices)
+%EMAILFEATURES takes in a word_indices vector and produces a feature vector
+%from the word indices
+%   x = EMAILFEATURES(word_indices) takes in a word_indices vector and 
+%   produces a feature vector from the word indices. 
+
+x(word_indices) = 1
+
+
+
+% =========================================================================
+    
+
+end
